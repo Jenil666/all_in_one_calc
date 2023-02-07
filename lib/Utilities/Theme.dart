75afba7e4075d58b90955749f2845d357d8b1a2f
RRwW <rw@RWs-Mac-Pro.local>
2023-02-07 11:47:38 +0530

    Initial commit

---
+++ lib/Utilities/Theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData LightMode = ThemeData(
+  appBarTheme: AppBarTheme(
+    color: Colors.black,
+        centerTitle: true,
+    backgroundColor: Colors.red,
+  ),
+  textTheme:  TextTheme(
+    headline1: TextStyle(),
+  )
+);
+
+
+
+ThemeData Darktheam = ThemeData(
+    appBarTheme: AppBarTheme(
+      color: Colors.black,
+      centerTitle: true,
+      backgroundColor: Colors.red,
+    ),
+    textTheme:  TextTheme(
+      headline1: TextStyle(),
+    )
+);
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.black
+        )
+      ),
+      routes:{
+
+      },
+    )
+  );
+}
